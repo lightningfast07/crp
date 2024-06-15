cdf03e1dc6824e5514b783d3474d473d6c9edd15
lightningfast07 <108779360+lightningfast07@users.noreply.github.com>
2024-06-15 20:07:42 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UploadComponent from "./Components/UploadComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Excel Prediction App</h1>
+        <UploadComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
